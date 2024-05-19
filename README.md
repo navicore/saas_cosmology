239670679ac698acd1d4ca2240201567b57f6fd7
Ed Sweeney <ed@onextent.com>
2024-05-19 12:42:51 -0700

    init

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 
 ![alt text](diagrams/saas_cosmology_1.png)
-
-![alt text](saas_cosmology_2.png "Title")
-
-![alt text](saas_cosmology_3.png "Title")
-
-![alt text](saas_cosmology_4.png "Title")
-
-![alt text](saas_cosmology_5.png "Title")
-
-![alt text](saas_cosmology_6.png "Title")
-
-![alt text](saas_cosmology_7.png "Title")
-
-![alt text](saas_cosmology_8.png "Title")
+![alt text](diagrams/saas_cosmology_2.png)
+![alt text](diagrams/saas_cosmology_3.png)
+![alt text](diagrams/saas_cosmology_4.png)
+![alt text](diagrams/saas_cosmology_5.png)
+![alt text](diagrams/saas_cosmology_6.png)
+![alt text](diagrams/saas_cosmology_7.png)
+![alt text](diagrams/saas_cosmology_8.png)
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-dot -Tpng saas_cosmology_1.dot -o saas_cosmology_1.png
-dot -Tpng saas_cosmology_2.dot -o saas_cosmology_2.png
-dot -Tpng saas_cosmology_3.dot -o saas_cosmology_3.png
-dot -Tpng saas_cosmology_4.dot -o saas_cosmology_4.png
-dot -Tpng saas_cosmology_5.dot -o saas_cosmology_5.png
-dot -Tpng saas_cosmology_6.dot -o saas_cosmology_6.png
-dot -Tpng saas_cosmology_7.dot -o saas_cosmology_7.png
-dot -Tpng saas_cosmology_8.dot -o saas_cosmology_8.png
+dot -Tpng diagrams/saas_cosmology_1.dot -o diagrams/saas_cosmology_1.png
+dot -Tpng diagrams/saas_cosmology_2.dot -o diagrams/saas_cosmology_2.png
+dot -Tpng diagrams/saas_cosmology_3.dot -o diagrams/saas_cosmology_3.png
+dot -Tpng diagrams/saas_cosmology_4.dot -o diagrams/saas_cosmology_4.png
+dot -Tpng diagrams/saas_cosmology_5.dot -o diagrams/saas_cosmology_5.png
+dot -Tpng diagrams/saas_cosmology_6.dot -o diagrams/saas_cosmology_6.png
+dot -Tpng diagrams/saas_cosmology_7.dot -o diagrams/saas_cosmology_7.png
+dot -Tpng diagrams/saas_cosmology_8.dot -o diagrams/saas_cosmology_8.png
