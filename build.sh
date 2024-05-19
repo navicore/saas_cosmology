e7ac9e950fef6b1960383bb16e7ada99f4084ed4
Ed Sweeney <ed@onextent.com>
2024-05-19 15:31:37 -0700

    size

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-dot -Tpng diagrams/saas_cosmology_1.dot -o diagrams/saas_cosmology_1.png
-dot -Tpng diagrams/saas_cosmology_2.dot -o diagrams/saas_cosmology_2.png
-dot -Tpng diagrams/saas_cosmology_3.dot -o diagrams/saas_cosmology_3.png
-dot -Tpng diagrams/saas_cosmology_4.dot -o diagrams/saas_cosmology_4.png
-dot -Tpng diagrams/saas_cosmology_5.dot -o diagrams/saas_cosmology_5.png
-dot -Tpng diagrams/saas_cosmology_6.dot -o diagrams/saas_cosmology_6.png
-dot -Tpng diagrams/saas_cosmology_7.dot -o diagrams/saas_cosmology_7.png
-dot -Tpng diagrams/saas_cosmology_8.dot -o diagrams/saas_cosmology_8.png
+# width,height,ignore aspect ratio
+GSIZE="7,3!"
+
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_1.dot -o diagrams/saas_cosmology_1.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_2.dot -o diagrams/saas_cosmology_2.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_3.dot -o diagrams/saas_cosmology_3.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_4.dot -o diagrams/saas_cosmology_4.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_5.dot -o diagrams/saas_cosmology_5.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_6.dot -o diagrams/saas_cosmology_6.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_7.dot -o diagrams/saas_cosmology_7.png
+dot -Tpng -Gsize=$GSIZE -Gdpi=300 diagrams/saas_cosmology_8.dot -o diagrams/saas_cosmology_8.png
